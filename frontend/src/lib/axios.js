6108caff450f6f4ed61776c4c82f525ef7002e8e
Gaurav Gupta <125338113+Gaurav075@users.noreply.github.com>
2025-06-01 07:49:12 +0000

    done

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "https://chat-app-c6pa.onrender.com/api" : "/api",
+    // baseURL: import.meta.env.MODE === "development" ? "https://chat-app-c6pa.onrender.com/api" : "/api",
+    baseURL: "https://chat-app-c6pa.onrender.com/api",
     withCredentials:true,
 })
